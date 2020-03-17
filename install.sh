f58afc4222175936ca90c0236a4450e384deddfd
b0th <43070774+b0th@users.noreply.github.com>
2020-03-17 12:32:43 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install i3
+sudo apt-get install i3-wm dunst i3lock i3status suckless-tools
+sudo apt-get install compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior
+
+git clone https://github.com/turquoise-hexagon/cherry.git
+cd cherry ; ./make.sh
+fc-cache -fv
+
