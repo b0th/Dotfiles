a910c1ccff56e466602d9a62d61ceb382e46ea53
b0th <43070774+b0th@users.noreply.github.com>
2020-03-18 19:23:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt-get install i3-wm dunst i3lock i3status suckless-tools
 sudo apt-get install compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior
 
 git clone https://github.com/turquoise-hexagon/cherry.git
-cd cherry ; ./make.sh
+cd cherry
+chmod +x make.sh ; ./make.sh
 fc-cache -fv
 
