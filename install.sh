3d79a767238311175a5f2aa13e78612c70796c75
b0th <43070774+b0th@users.noreply.github.com>
2020-06-28 17:47:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@ sudo apt-get install i3
 sudo apt-get install i3-wm dunst i3lock i3status suckless-tools
 sudo apt-get install compton hsetroot rxvt-unicode xsel rofi fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior
 
-#Rename to dotfile format
-mv config/ .config/
-mv urxvt/ .urxvt/
-
 #Move to $HOME
 mv .* * $HOME
 
@@ -15,3 +11,6 @@ git clone https://github.com/turquoise-hexagon/cherry.git
 cd cherry
 chmod +x make.sh ; ./make.sh
 fc-cache -fv
+
+#Other fonts
+#https://int10h.org/oldschool-pc-fonts/download/ultimate_oldschool_pc_font_pack_v1.01.zip
